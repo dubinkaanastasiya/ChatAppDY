d098588f6290523bce3d88b646b3304693f329a3
Anastasiya <dubinka.anastasya@yandex.ua>
2016-12-09 23:38:49 +0200

    Add files via upload

--- src/Command.java
+++ src/Command.java
@@ -1,29 +1,22 @@
-public abstract class Command {
+public abstract class Command
+{
 }
 
-class MessageCommand extends Command {
+class MessageCommand extends Command
+{
     String message;
 
-    MessageCommand(String message) {
+    MessageCommand(String message)
+    {
         this.message = message;
     }
 }
 
-class DisconnectCommand extends Command {
-}
-
 class NickCommand extends Command {
     String nick;
-    boolean busy;
 
-    NickCommand(String nick, boolean busy) {
+    NickCommand(String nick)
+    {
         this.nick = nick;
-        this.busy = busy;
     }
-}
-
-class AcceptCommand extends Command {
-}
-
-class RejectCommand extends Command {
 }
